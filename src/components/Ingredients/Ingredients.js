c51dcf0e2162ecda83f47ef827463749f5e220fc
naekit <mykyta.onishchuck@code.berlin>
2023-01-30 18:33:44 -0500

    some example of useState for working with lists

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 
 import IngredientForm from "./IngredientForm"
+import IngredientList from "./IngredientList"
 import Search from "./Search"
 
-function Ingredients() {
+const Ingredients = () => {
+	const [ingredients, setIngredients] = useState([])
+
+	const addIngredientHandler = (ingredient) => {
+		setIngredients((prevIngredients) => [
+			...prevIngredients,
+			{ id: Math.random().toString(), ...ingredient },
+		])
+	}
+
+	const removeIngredientHandler = (ingredientId) => {
+		setIngredients((prevIngredients) =>
+			prevIngredients.filter(
+				(ingredient) => ingredient.id !== ingredientId
+			)
+		)
+	}
+
 	return (
 		<div className="App">
-			<IngredientForm />
+			<IngredientForm addIngredient={addIngredientHandler} />
 
 			<section>
 				<Search />
-				{/* Need to add list here! */}
+				<IngredientList
+					onRemoveItem={removeIngredientHandler}
+					ingredients={ingredients}
+				/>
 			</section>
 		</div>
 	)
